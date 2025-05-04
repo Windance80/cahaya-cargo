5333562d60442daf4a7000c28b302e3423b0c386
sandy <sandy.theodorus@gmail.com>
2025-05-04 20:09:11 +0700

    add w-full in youtube embed

--- app/components/VideoEmbed.js
+++ app/components/VideoEmbed.js
@@ -1,5 +1,5 @@
 export default function VideoEmbed() {
     return (
-        <iframe className="flex-auto md:flex-1" loading="lazy" width="518" height="315" src="https://www.youtube.com/embed/5EuLfPYE4tw?si=_qbXrQ4ku22ofYPl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
+        <iframe className="flex-auto md:flex-1 w-full" loading="lazy" width="518" height="315" src="https://www.youtube.com/embed/5EuLfPYE4tw?si=_qbXrQ4ku22ofYPl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
     );
 }
